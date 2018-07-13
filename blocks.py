cdcf713d52e07dde69d1017b244009f3897826ac
David27737 <41197507+David27737@users.noreply.github.com>
2018-07-13 10:55:34 -0400

    Create blocks.py

---
+++ blocks.py
@@ -0,0 +1,9 @@
+"""
+blocks class
+idk what this game is about
+not finished
+"""
+class Block(class):
+  def __init__(self, movement_points, strength):
+    self.movement_points = movement_points
+    self.strength = strength
