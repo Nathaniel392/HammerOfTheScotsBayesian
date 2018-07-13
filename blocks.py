cc8bc380b3be78c81ab08940c837c3dd14ba53ce
David27737 <41197507+David27737@users.noreply.github.com>
2018-07-13 11:17:56 -0400

    Update blocks.py

--- blocks.py
+++ blocks.py
@@ -1,9 +1,11 @@
+import country
 """
 blocks class
 idk what this game is about
 not finished
 """
 class Block(class):
-  def __init__(self, movement_points, strength):
+  def __init__(self, movement_points, strength, country_str):
     self.movement_points = movement_points
     self.strength = strength
+    self.country = country.Country(country_str)
