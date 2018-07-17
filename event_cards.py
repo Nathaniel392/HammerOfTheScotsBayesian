92a60bfcaba5614eb70c5c6d52fda4ca6b0c2821
David27737 <41197507+David27737@users.noreply.github.com>
2018-07-17 08:15:55 -0400

    Update event_cards.py

--- event_cards.py
+++ event_cards.py
@@ -4,3 +4,9 @@ event cards inheret freom Card
 not finished
 """
 class EventCard(Card):
+    def __init__(self, text):
+        self.text = text
+    def __str__(self):
+        if not self.hidden:
+            return self.text
+   
