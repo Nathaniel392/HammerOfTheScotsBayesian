5532acdf48ca43986a846f8f551e745839f4c558
David27737 <41197507+David27737@users.noreply.github.com>
2018-07-17 08:13:05 -0400

    Update movement_cards.py

--- movement_cards.py
+++ movement_cards.py
@@ -6,3 +6,8 @@ not finshed
 """
 
 class MovementCard(Card):
+    def __init__(self, movement_points):
+        self.movement_points = movement_points
+    def __str__(self):
+        if not self.hidden:
+            return 'Movement card with ' , movement_points , ' movement points'
