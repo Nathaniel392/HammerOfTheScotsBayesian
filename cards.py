a039feef90933e1bdedce8305438068015f5ead2
sidsomasi <41197489+sidsomasi@users.noreply.github.com>
2018-07-17 15:21:06 -0400

    Create cards.py

---
+++ cards.py
@@ -0,0 +1,15 @@
+class Card( object ):
+  
+  #rank is an int 1-3 for number cards, 4 for event cards.
+  
+  def __init__ ( self, rank=0, points=0, event=" " ):
+  
+    self.__rank = 0
+    self.__points = 0
+    self.__event = " "
+    
+    if type(rank) == int and type(points) == int and type(event) = str:
+    
+    
+    
+  
