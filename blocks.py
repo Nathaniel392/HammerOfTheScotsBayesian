25a58c658a92db1c43a6c209195e3318ee5462f1
elliotmoore1 <41197493+elliotmoore1@users.noreply.github.com>
2018-07-18 10:14:11 -0400

    Update blocks.py

--- blocks.py
+++ blocks.py
@@ -8,8 +8,8 @@ as well as edward
 as well as king
 """
 class Block(object):
-    def __init__(self, name, movement_points, attack_letter, attack_number, attack_strength1, attack_strength2, attack_strength3 \
-                attack_strength4, allegiance, location, has_cross, type_men = None):
+    def __init__(self, name, movement_points, attack_letter, attack_number, initial_attack_strength, \
+                allegiance, location, has_cross, type_men = None):
         """
         name is name of object
         movement_points is movement points
@@ -25,8 +25,8 @@ class Block(object):
         self.movement_points = movement_points
         self.attack_letter = attack_letter
         self.attack_number = attack_number
-        self.attack_strength = list(attack_strength1, attack_strength2, attack_strength3, attack_strength4)
-        self.current_strength = attack_strength4
+        self.attack_strength = initial_attack_strength
+        self.current_strength = initial_attack_strength
         self.location = location
         self.allegiance = allegiance
         self.has_cross = has_cross
@@ -37,25 +37,22 @@ class Block(object):
         returns False if block is dead
         otherwise returns True and damages block
         """
-        if self.current_strength == self.attack_strength[0] or self.current_strength == 1:
+        if self.current_strength == 1:
             return False
         else:
-            for i, strength in enumerate(self.attack_strength):
-                if self.current_strength = strength:
-                    self.current_strength = attack_strength[i - 1]
-                    return True
+            self.current_strength -=1
+            return True
     def heal(self, health_points):
         """
         returns False if cannot heal by that many points
         otherwise returns True and heals block
         """
-        for i, strength in enumerate(self.attack_strength):
-            if self.current_strength = strength:
-                if i + health_points - 1 > len(self.attack_strength):
-                    return False
-                else:
-                    self.current_strength = self.attack_strength[i + health_points]
-                    return True
+        
+        if self.current_strength + health_points > self.attack_strength:
+            return False
+        else:
+            self.current_strength = self.attack_strength + health_points
+            return True
     def move(self, region):
         """
         supposed to move block
@@ -66,37 +63,37 @@ class Edward(Block):
     english king block
     """
 
-    def __init__(self, name, movement_points, attack_letter, attack_number, attack_strength1, attack_strength2, attack_strength3 \
-                attack_strength4, allegiance, location, has_cross):
-        super().__init__(name, movement_points, attack_letter, attack_number, attack_strength1, attack_strength2, attack_strength3 \
-                attack_strength4, allegiance, location, has_cross)
+    def __init__(self, name, movement_points, attack_letter, attack_number, initial_attack_strength, \
+                 allegiance, location, has_cross):
+        super().__init__(name, movement_points, attack_letter, attack_number, initial_attack_strength, \
+                 allegiance, location, has_cross)
 class Wallace(Block):
     """
     wallace block
     """
-    def __init__(self, name, movement_points, attack_letter, attack_number, attack_strength1, attack_strength2, attack_strength3 \
-                attack_strength4, allegiance, location, has_cross):
-        super().__init__(name, movement_points, attack_letter, attack_number, attack_strength1, attack_strength2, attack_strength3 \
-                attack_strength4, allegiance, location, has_cross)
+    def __init__(self, name, movement_points, attack_letter, attack_number, initial_attack_strength, \
+                 allegiance, location, has_cross):
+        super().__init__(name, movement_points, attack_letter, attack_number, initial_attack_strength, \
+                 allegiance, location, has_cross)
 
 class ScottishKing(Block):
     """
     Scottish King
     """
-    def __init__(self, name, movement_points, attack_letter, attack_number, attack_strength1, attack_strength2, attack_strength3 \
-                attack_strength4, allegiance, location, has_cross):
-        super().__init__(name, movement_points, attack_letter, attack_number, attack_strength1, attack_strength2, attack_strength3 \
-                attack_strength4, allegiance, location, has_cross)
+    def __init__(self, name, movement_points, attack_letter, attack_number, initial_attack_strength, \
+                 allegiance, location, has_cross):
+        super().__init__(name, movement_points, attack_letter, attack_number, initial_attack_strength, \
+                 allegiance, location, has_cross)
 
 
 class Noble(Block):
     """
     adds extra attribute home_location on top of Block
     """
-    def __init__(self, name, movement_points, attack_letter, attack_number, attack_strength1, attack_strength2, attack_strength3 \
-                attack_strength4, allegiance, location, has_cross, home_location):
-        super().__init__(name, movement_points, attack_letter, attack_number, attack_strength1, attack_strength2, attack_strength3 \
-                attack_strength4, allegiance, location, has_cross)
+    def __init__(self, name, movement_points, attack_letter, attack_number, intitial_attack_strength,\
+                 allegiance, location, has_cross, home_location):
+        super().__init__(name, movement_points, attack_letter, attack_number, initial_attack_strength, \
+                 allegiance, location, has_cross)
         self.home_location = home_location
     def go_home(self):
         self.location = self.home_location
@@ -118,18 +115,18 @@ class Norse(Block):
     """
     norse block
     """
-    def __init__(self, name, movement_points, attack_letter, attack_number, attack_strength1, attack_strength2, attack_strength3 \
-                attack_strength4, allegiance, location, has_cross):
-        super().__init__(name, movement_points, attack_letter, attack_number, attack_strength1, attack_strength2, attack_strength3 \
-                attack_strength4, allegiance, location, has_cross)
+    def __init__(self, name, movement_points, attack_letter, attack_number, initial_attack_strength,\
+                 allegiance, location, has_cross):
+        super().__init__(name, movement_points, attack_letter, attack_number, initial_attack_strength,\
+                         allegiance, location, has_cross)
 class Celtic(Block):
     """
     celtic block
     """
-    def __init__(self, name, movement_points, attack_letter, attack_number, attack_strength1, attack_strength2, attack_strength3 \
-                attack_strength4, allegiance, location, has_cross, type_men):
-        super().__init__(name, movement_points, attack_letter, attack_number, attack_strength1, attack_strength2, attack_strength3 \
-                attack_strength4, allegiance, location, has_cross, type_men)
+    def __init__(self, name, movement_points, attack_letter, attack_number, initial_attack_strength,\
+                 allegiance, location, has_cross, type_men):
+        super().__init__(name, movement_points, attack_letter, attack_number, initial_attack_strength,\
+                         allegiance, location, has_cross, type_men)
 
 
 
