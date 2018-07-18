bf651ad8ab9a3828ccec561f85c209484239cbe5
David27737 <41197507+David27737@users.noreply.github.com>
2018-07-18 10:52:57 -0400

    Update dice.py

--- dice.py
+++ dice.py
@@ -3,11 +3,10 @@ import random
 Dice.num_dice(num_dice) returns tuple of dice things
 """
 dice_list = list()
-class Dice(object):
-    @staticmethod
-    def roll(num_dice):
-        for i in range(num_dice):
-            dice_list.append(random.randint(1,6))
-        return tuple(dice_list)
+
+def roll(num_dice):
+    for i in range(num_dice):
+        dice_list.append(random.randint(1,6))
+    return tuple(dice_list)
  
     
