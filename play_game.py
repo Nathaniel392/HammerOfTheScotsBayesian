081b43ddfc1022a1dd4368618a1153ca5aa185bc
David27737 <41197507+David27737@users.noreply.github.com>
2018-07-18 13:13:37 -0400

    Update play_game.py

--- play_game.py
+++ play_game.py
@@ -1,1 +1,22 @@
+import cards
+import initialize_blocks
+import static_locations
 
+"""
+stuff
+"""
+def play_game():
+    #create stuff
+    deck = Deck()
+    nobles, non_noble_blocks = initilialize_blocks.initialize_blocks()
+    static_locations = static_locations.create_static_locations()
+    dynamic_locations = static_locations.create_static_locations()
+    
+    #deal hands
+    my_hand = list()
+    opp_hand = list()
+    for i in range(5):
+        my_hand.append(Deck.deal())
+        opp_hand.append(Deck.deal())
+    
+play_game()
