a1730e411720cc738634f43373799b5f0e85eb1d
David27737 <41197507+David27737@users.noreply.github.com>
2018-07-18 08:11:15 -0400

    Update blocks.py

--- blocks.py
+++ blocks.py
@@ -4,7 +4,7 @@ as well as noble class
 """
 class Block(object):
     def __init__(self, name, movement_points, attack_letter, attack_number, attack_strength1, attack_strength2, attack_strength3 \
-                attack_strength4):
+                attack_strength4, location):
         """
         name is name of object
         movement_points is movement points
@@ -13,14 +13,24 @@ class Block(object):
         attack_strength1 is weakest attack strength or 0 
         attack_strength4 is strongest attack strength
         current_strength is current strength
-        
+        location is a Region object
         """
         self.name = name
         self.movement_points = movement_points
         self.attack_letter = attack_letter
         self.attack_number = attack_number
-        self.attack_strength1 = attack_strength1
-        self.attack_strength2 = attack_strength2
-        self.attack_strength3 = attack_strength3
-        self.attack_strength4 = attack_strength4
+        self.attack_strength = list(attack_strength1, attack_strength2, attack_strength3, attack_strength4)
         self.current_strength = attack_strength4
+        self.location = location
+    def get_hurt(self, damage):
+        """
+        returns False if block is dead
+        otherwise returns True and damages block
+        """
+        if self.current_strength == self.attack_strength[0] or self.current_strength == 1:
+            return False
+        else:
+            for i, strength in enumerate(self.attack_strength):
+                if self.current_strength = strength:
+                    self.current_strength = attack_strength[i - 1]
+                    return True
