0a8ff6255dfcfd6db52536f608b62a8c8353cbc7
David27737 <41197507+David27737@users.noreply.github.com>
2018-07-18 15:53:14 -0400

    Update blocks_occupied.py

--- blocks_occupied.py
+++ blocks_occupied.py
@@ -10,4 +10,16 @@ def blocks_occupied():
       
     return territory_lst
 def make_occupied():
+    """
+    initialize where all blocks are
+    """
+    nobles, other_blocks = initialize_blocks.initialize_blocks()
+    territory_lst = blocks_occupied()
+    for block in nobles:
+        territory_lst[block.location].append(block)
+    for block in other_blocks:
+        territory_lst[block.location].append(block)
+    print(territory_lst)
+    return territory_lst
+make_occupied()
     
