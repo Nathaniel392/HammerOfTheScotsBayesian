67beb423f117835ccc95c82c0f7af8e2f112f842
Nathaniel392 <33849443+Nathaniel392@users.noreply.github.com>
2018-07-24 16:07:39 -0400

    Add files via upload

--- blocks.py
+++ blocks.py
@@ -10,7 +10,7 @@ Created on Wed Jul 18 10:51:35 2018
 blocks class
 as well as noble class
 """
-import random
+
 
 class Block(object):
     def __init__(self, name = None, movement_points = None, attack_letter = None , attack_number = None, max_attack_strength = None, \
@@ -23,9 +23,9 @@ class Block(object):
         attack_strength1 is weakest attack strength or 0
         attack_strength4 is strongest attack strength
         current_strength is current strength
+        location is a Region object
         type_men is usually archers,knights, or infantry
         """
-        
         self.name = name
         self.movement_points = movement_points
         self.attack_letter = attack_letter
@@ -34,9 +34,11 @@ class Block(object):
         self.current_strength = max_attack_strength
         self.allegiance = allegiance
         self.has_cross = has_cross
-        self.block_ID = block_ID
-        self.type = type_men
+        self.blockID = blockID
         
+        if type_men != None:
+            self.type = type_men
+
     def get_hurt(self, damage):
         """
         returns False if block is dead
@@ -59,6 +61,11 @@ class Block(object):
             self.current_strength = self.attack_strength + health_points
             return True
           
+    def move(self, region, block):
+        """
+        supposed to move block to a adjacent location and take away a movement point
+        """
+        pass
     def __repr__(self):
         """
         prints name
@@ -66,38 +73,32 @@ class Block(object):
         return('name: ' + str(self.name))
     def __len__(self):
         return 1
-      
-    def is_dead(self):
-        return self.current_strength == 0
+
 
 class Noble(Block):
     """
     adds extra attribute home_location on top of Block
     """
-    
-    def __init__(self, name = None, movement_points = None, attack_letter = None, attack_number = None, max_attack_strength = None,\
-                 has_cross = None, block_ID = None, home_location = None, loyalty = None, allegiance = None):
+    def __init__(self, name, movement_points, attack_letter, attack_number, max_attack_strength,\
+                 has_cross, block_ID, home_location, loyalty, allegiance):
 
         super(Noble, self).__init__(name, movement_points, attack_letter, attack_number, max_attack_strength, \
                  has_cross, block_ID, allegiance)
-        #self.home_location = home_location
+
+        self.home_location = home_location
         self.loyalty = loyalty
 
     def change_allegiance(self, allegiance = None):
         """
-        Changes noble's alliegance.
-        No paramater: flips the noble's alliegance
-        Paramater alliegance: changes alliegance to the given parameter
+        if no allegiance passed, changes 
+        allegiance is allegiance to change to
+        whether it is changed or not
         """
-        
-        #Moray can't change sides
         if self.has_cross:
-            pass
-        else:
-            if allegiance == None:
-                if self.allegiance == 'SCOTLAND':
-                    self.allegiance = 'ENGLAND'
-                else:
-                    self.allegiance == 'SCOTLAND'
+            raise Exception("Moray can't change sides")
+        if allegiance == None:
+            if self.allegiance == 'SCOTLAND':
+                self.allegiance = 'ENGLAND'
             else:
-                self.allegiance = allegiance
+                self.allegiance == 'SCOTLAND'
+        self.allegiance = allegiance
--- initialize_blocks.py
+++ initialize_blocks.py
@@ -26,7 +26,7 @@ def read_file(file_name):
 	output = []
 	fp.readline()
 	for line in fp:
-		info = line.strip()
+		info = line.strip().split()
 		output.append(info)
 	
 	# (2D) list of information from file
@@ -34,9 +34,11 @@ def read_file(file_name):
 	return output
 
 
-def initialize_blocks():
+def initialize_blocks(scenario):
     '''
-
+    Initialized all blocks into one list
+    scenario:  setup scenario: 1=Braveheart/Campaign, 2=Bruce
+    Returns:  List of all blocks in the game, with no alliegance
     '''
     block_list = []
     block_stats = read_file('block_stats.txt')
@@ -44,132 +46,46 @@ def initialize_blocks():
     # Read in information about each block
     for info_line in block_stats:
         name = info_line[0]
-        movement_points = info_line[1]
+        movement_points = int(info_line[1])
         attack_letter = info_line[2]
-        attack_number = info_line[3]
-        attack_strength = info_line[4]
+        attack_number = int(info_line[3])
+        attack_strength = int(info_line[4])
         block_type = info_line[5]
         cross = info_line[6]
-        block_id = info_line[7]
+        block_id = int(info_line[7])
 
-        # Add the home attribute if it's a noble
+        # Check if it's a noble
         if block_type == 'BRUCE' or block_type == 'COMYN':
+            is_noble = True
+
+            # Set up noble homes
             home = info_line[8]
+            if home.isdigit():
+                home = int(home)
+            else: #For Bruce and Comyn, home is a tuple - must convert
+                home = home.strip('()')
+                home = home.split(',')
+                for index, region in enumerate(home):
+                    home[index]= int(region)
 
+            # Assign noble loyalty
+            loyalty = block_type
+            temp_block = blocks.Noble(name, movement_points, attack_letter, attack_number, attack_strength, cross, block_id)
 
+        else:
+            is_noble = False
+            temp_block = blocks.Block(name, movement_points, attack_letter, attack_number, attack_strength, cross, block_type, block_id, home, loyalty)
 
+        #Finished block - no alliegence yet
+        block_list.append(temp_block)
 
+    return block_list
 
-def initialize_blocks_braveheart():
-    """
-    This function initializes all the block objects into two
-    lists, one for all 14 nobles and the other for the rest
-    of the infantry and other block
-    """
-    #Creates a list of every block potentially in the game
-    data = []
-    data = read_file('block_stats.txt')
-    for entry in data:
+def main():
+    initialize_blocks()
 
-
-
-    nobles = []
-    other_blocks = []
-    specific_data = []
-
-    #Initialize Infantry, Knights, Archers Objects.
-    for i in range(22):
-        specific_data = data[i].split()
-
-        for index, item in enumerate(specific_data):
-            if type(item) is int:
-                specific_data[index] = int(item)
-        
-        other_blocks.append(blocks.Block(specific_data[0], specific_data[1], specific_data[2], specific_data[3], specific_data[4], specific_data[5], specific_data[6], specific_data[7], specific_data[8]))
-        #other_blocks.append(blocks.Block(for data in specific_data))
-    #Initilize Norse Object
-    specific_data = data[22].split()
-    other_blocks.append(blocks.Norse(specific_data[0], specific_data[1], specific_data[2], specific_data[3], specific_data[4], specific_data[5], specific_data[6], specific_data[7]))
-    #Initilize Celtic (Wales and Ulsher)
-    for i in range(23, 26):
-        specific_data = data[i].split()
-        other_blocks.append(blocks.Celtic(specific_data[0], specific_data[1], specific_data[2], specific_data[3], specific_data[4], specific_data[5], specific_data[6], specific_data[7], specific_data[8]))
-    #Initialize Wallace Object
-    specific_data = data[26].split()
-    other_blocks.append(blocks.Wallace(specific_data[0], specific_data[1], specific_data[2], specific_data[3], specific_data[4], specific_data[5], specific_data[6], specific_data[7]))
-
-    #Initialize Edward Object
-    specific_data = data[27].split()
-    other_blocks.append(blocks.Edward(specific_data[0], specific_data[1], specific_data[2], specific_data[3], specific_data[4], specific_data[5], specific_data[6], specific_data[7]))
-
-    #Initialize Scottish King Object
-    specific_data = data[28].split()
-    other_blocks.append(blocks.ScottishKing(specific_data[0], specific_data[1], specific_data[2], specific_data[3], specific_data[4], specific_data[5], specific_data[6], specific_data[7]))
-
-    #Initialize Noble Objects
-    for i in range(14):
-        i+=29
-        specific_data = data[i].split()
-        nobles.append(blocks.Noble(specific_data[0], specific_data[1], specific_data[2], specific_data[3], specific_data[4], specific_data[5], specific_data[6],    \
-                            specific_data[7], specific_data[8], specific_data[9]))
-        i-=29
-
-    static_nobles = copy.deepcopy(nobles)
-    static_other_blocks = copy.deepcopy(other_blocks)
-        
-    return nobles, other_blocks, static_nobles, static_other_blocks
-
-def initialize_blocks_bruce():
-    """
-    This function initlializes all the block objects into two
-    lists, one for all 13 nobles and the other for the rest
-    of the infantry and other blocks
-    """
-    data = []
-    data = read_file('block_info_bruce.txt')
-
-    nobles = []
-    other_blocks = []
-    specific_data = []
-
-    #Initialize Infantry, Knights, Archers Objects.
-    for i in range(22):
-        specific_data = data[i].split()
-        
-        other_blocks.append(blocks.Block(specific_data[0], int(specific_data[1]), specific_data[2], int(specific_data[3]), int(specific_data[4]), specific_data[5], int(specific_data[6]), specific_data[7], specific_data[8]))
-    #Initilize Norse Object
-    specific_data = data[22].split()
-    other_blocks.append(blocks.Norse(specific_data[0], int(specific_data[1]), specific_data[2], int(specific_data[3]), int(specific_data[4]), specific_data[5], int(specific_data[6]), specific_data[7]))
-    #Initilize Celtic (Wales and Ulsher)
-    for i in range(23, 26):
-        specific_data = data[i].split()
-        other_blocks.append(blocks.Celtic(specific_data[0], int(specific_data[1]), specific_data[2], int(specific_data[3]), int(specific_data[4]), specific_data[5], int(specific_data[6]), specific_data[7], specific_data[8]))
-    #Initialize Wallace Object
-    specific_data = data[26].split()
-    other_blocks.append(blocks.Wallace(specific_data[0], int(specific_data[1]), specific_data[2], int(specific_data[3]), int(specific_data[4]), specific_data[5], int(specific_data[6]),specific_data[7]))
-
-    #Initialize Edward Object
-    specific_data = data[27].split()
-    other_blocks.append(blocks.Edward2(specific_data[0], int(specific_data[1]), specific_data[2], int(specific_data[3]), int(specific_data[4]), specific_data[5], int(specific_data[6]),specific_data[7]))
-
-    #Initialize Scottish King Object
-    specific_data = data[28].split()
-    other_blocks.append(blocks.ScottishKing(specific_data[0], int(specific_data[1]), specific_data[2], int(specific_data[3]), int(specific_data[4]), specific_data[5], int(specific_data[6]),specific_data[7]))
-
-    #Initialize Noble Objects
-    for i in range(14):
-        i+=29
-        specific_data = data[i].split()
-        nobles.append(blocks.Noble(specific_data[0], int(specific_data[1]), specific_data[2], int(specific_data[3]), int(specific_data[4]), specific_data[5], int(specific_data[6]),\
-                            specific_data[7], specific_data[8], specific_data[9]))
-        i-=29
-
-    static_nobles = copy.deepcopy(nobles)
-    static_other_blocks = copy.deepcopy(other_blocks)
-        
-    return nobles, other_blocks, static_nobles, static_other_blocks
-
-						
+if __name__ == '__main__':
+    main()	
 						
 
 
