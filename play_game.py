8c036bc24e12cab6001f498782823b7477d6f07d
elliotmoore1 <41197493+elliotmoore1@users.noreply.github.com>
2018-07-25 15:10:28 -0400

    Update play_game.py

--- play_game.py
+++ play_game.py
@@ -3,6 +3,7 @@ import initialize_blocks
 import location_prob
 import blocks_occupied
 import board
+import cardplay
 
 def prompt_scenario():
     '''
@@ -36,6 +37,7 @@ def prompt_ai_side():
     return the computer's role ('ENGLAND' or 'SCOTLAND')
     '''
     flag = False
+    opp_role = ''
     while not flag:
         computer_role = input("Enter which side you want the computer to play (England or Scotland): ")
         computer_role = computer_role.upper()
@@ -43,8 +45,58 @@ def prompt_ai_side():
             flag = True
         else:
             print("Invalid input, try again")
+    if computer_role == 'ENGLAND':
+        opp_role = 'SCOTLAND'
+    else:
+        opp_role = 'ENGLAND'
+    return computer_role, opp_role
 
-    return computer_role
+def opp_card_choice(cards):
+    '''
+    Print out all of the humans cards
+    Returns the index of the card in which they want
+    to play
+    '''
+    choice = ''
+    #Print out all the card possibilities
+    for card in cards:
+        if card == '1' or card == '2' or card == '3':
+            print(card, end = " ")
+        elif card == 'SEA':
+            print('SEA', end = " ")
+        elif card == 'HER':
+            print('HER')
+        elif card == 'PIL':
+            print('PIL')
+        elif card == 'TRU':
+            print('TRU')
+        elif card == 'VIC':
+            print('VIC')
+    #Input and error check what card the user wants to play
+    while choice not in cards:
+        choice = input('Enter the card you want to play: ')
+        choice = choice.upper()
+        for i,card in enumerate(cards):
+            if card == choice:
+                return(i)
+
+def resolve_card_opp(card, opp_role):
+    '''
+    This function takes two parameters. One is the card itself and
+    the other is the side in which the opponent is on. The function
+    determines which card is being played and calls functions accordingly
+    to resolve the card
+    '''
+    pass
+
+def resolve_card_computer(card, comp_role):
+    '''
+    This function takes two parameters. One is the card itself and the
+    other is the side in which the computer is playing. The function determines
+    which card is being played and calls functions accordingly to resolve
+    the card
+    '''
+    pass
 
 def play_game():
     #Determine scenario
@@ -52,8 +104,9 @@ def play_game():
     scenario = 'BRAVEHEART'
 
     #Determine which side the computer plays:
-    #computer_role = prompt_ai_side()
+    #computer_role, opp_role = prompt_ai_side()
     computer_role = 'ENGLAND'
+    opp_role = 'SCOTLAND'
 
     stc_locations = location_prob.create_static_locations()
     dyn_locations = location_prob.create_static_locations()
@@ -82,6 +135,26 @@ def play_game():
 
     #Fill board with pieces
     current_board.fill_board(block_list, scenario)
+    
+    #Find out what card the human wants to play
+    opp_card = opp_hand(opp_card_choice(opp_hand))
+    #Remove card from human hand
+    opp_hand.remove(opp_card)
+    #Get card for computer
+    computer_card = cardplay.random_card(computer_hand)
+    #Remove card from computer hand
+    computer_hand.remove(computer_card)
+
+    if compare_cards(opp_card, computer_card, computer_role):
+        #Enter code to resolve computer card first
+        resolve_card_computer(computer_card, computer_role)
+        resolve_card_opp(opp_card, opp_role)
+
+    else:
+        #Enter code to resolve human card first
+        resolve_card_opp(opp_card, opp_role)
+        resolve_card_computer(computer_card, computer_role)
+
     
     #Print blocks for testing
     #for block in block_list:
