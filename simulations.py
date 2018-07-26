673ed145f0b3c28c8678883e35039c9f097fb901
David27737 <41197507+David27737@users.noreply.github.com>
2018-07-26 09:31:30 -0400

    Add files via upload

--- simulations.py
+++ simulations.py
@@ -1,7 +1,140 @@
-import combat
+
 import blocks
 import copy
 import random
+
+def organize(blocks):
+	'''
+	Separates list of blocks into a, b, and c
+	blocks:  List of blocks
+	Returns:  Dictionary of a-list, b-list, and c-list
+	'''
+	ordered = {'A':[], 'B':[], 'C':[]}
+
+	for block in blocks:
+		if block.attack_letter == 'A':
+			ordered['A'].append(block)
+		elif block.attack_letter == 'B':
+			ordered['B'].append(block)
+		elif block.attack_letter == 'C':
+			ordered['C'].append(block)
+
+	return ordered
+
+def find_max_strength(block_lst):
+	"""
+	finds max strength
+	returns list blocks with max strength
+	"""
+	max_strength = 0
+	not_found = True
+	while(not_found):
+		for block in block_lst:
+			not_found = False
+			if block.current_strength > max_strength:
+				max_strength = block.current_strength
+				not_found = True
+	strong_blocks = list()
+	for block in block_lst:
+		if block.current_strength == max_strength:
+			strong_blocks.append(block)
+	return strong_blocks
+
+def attack_block(attack_block_block, defending_blocks):
+	"""
+	attacks blocks
+	uses random in case where everyone same health
+	attack_block_block is the block attacking
+	defending_blocks is the list of all defending blocks to be attacked
+	"""
+
+	if defending_blocks == []:
+		return False
+	dice_lst = dice.roll(attack_block_block.current_strength)
+	
+
+
+
+	for num in dice_lst:
+
+		strong_blocks = find_max_strength(defending_blocks)
+
+		if num <= attack_block_block.attack_number:
+			block_to_get_hurt = strong_blocks[random.randint(0, len(strong_blocks) - 1)]
+		  	
+
+			block_to_get_hurt.get_hurt(1)
+			
+
+def check_if_dead(attackers_lst, defenders_lst, attack_reinforcements, defense_reinforcements, eng_pool = list(), scot_pool = list()):
+	"""
+	checkers if attackers and defenders are alive
+	moves nobles to different reinforcements
+	moves dead to the roster
+	if king dies then returns stuff
+	"""
+	attacker_is_dead = True
+	defender_is_dead = True
+	
+	for i, block in enumerate(attackers_lst):
+	
+
+		if block.type == 'KING' and block.is_dead():
+		
+			return True, False
+		elif block.has_cross and block.is_dead():
+			if current_board == None:
+				attackers_lst.pop(i)
+			else:
+				attackers_lst.pop(i)
+				
+		
+		elif type(block) == blocks.Noble and block.is_dead():
+			block.change_allegiance()
+	
+			defense_reinforcements.append(attackers_lst.pop(i))
+		elif type(block) != blocks.Noble and block.is_dead():
+			if block.allegiance == 'ENGLAND':
+				eng_pool.append(attackers_lst.pop(i))
+			else:
+				scot_pool.append(attackers_lst.pop(i))
+		
+			
+		if not block.is_dead():
+	
+			attacker_is_dead = False
+
+
+	for i, block in enumerate(defenders_lst):
+
+
+		
+		if block.type == 'KING' and block.is_dead():
+	
+			return False, True
+
+		elif block.has_cross and block.is_dead():
+
+			defenders_lst.pop(i)
+			
+
+		elif type(block) == blocks.Noble and block.is_dead():
+			
+			block.change_allegiance()
+
+			attack_reinforcements.append(defenders_lst.pop(i))
+		elif type(block) != blocks.Noble and block.is_dead():
+			if block.allegiance == 'ENGLAND':
+				eng_pool.append(defenders_lst.pop(i))
+			else:
+				scot_pool.append(defenders_lst.pop(i))
+			
+
+		if not block.is_dead():
+	
+			defender_is_dead = False
+
+	return attacker_is_dead, defender_is_dead
 
 def battle(attack, defense, attack_reinforcements = list(), defense_reinforcements = list(), before_letter = 'A', before_number = 0, turn = 'defender'):
 	'''
@@ -23,8 +156,8 @@ def battle(attack, defense, attack_reinforcements = list(), defense_reinforcemen
 	turn_found = False
 
 
-	attackers = combat.organize(attack)
-	defenders = combat.organize(defense)
+	attackers = organize(attack)
+	defenders = organize(defense)
 
 	attackers_allegiance = attack[0].allegiance
 	defenders_allegiance = defense[0].allegiance
@@ -41,21 +174,19 @@ def battle(attack, defense, attack_reinforcements = list(), defense_reinforcemen
 		else:
 			number_found = True
 
-
 			if combat_round >= 1:
 				
 
 				attack += attack_reinforcements
 				defense += defense_reinforcements
 
-
 				attack_reinforcements = list()
 				defense_reinforcements = list()
 				
 			
 				
-				attackers = combat.organize(attack)
-				defenders = combat.organize(defense)
+				attackers = organize(attack)
+				defenders = organize(defense)
 
 
 			for letter in 'ABC':
@@ -76,12 +207,12 @@ def battle(attack, defense, attack_reinforcements = list(), defense_reinforcemen
 										if random.randint(0,2) == 0:
 											attacking_block.current_strength = 0
 
-									combat.attack_block(attacking_block, attack)
+									attack_block(attacking_block, attack)
 
 
 
 					
-									attacker_is_dead, defender_is_dead = combat.check_if_dead(attack, defense, attack_reinforcements, defense_reinforcements)
+									attacker_is_dead, defender_is_dead = check_if_dead(attack, defense, attack_reinforcements, defense_reinforcements)
 							
 									if attacker_is_dead and combat_round != 0:
 										
@@ -98,10 +229,10 @@ def battle(attack, defense, attack_reinforcements = list(), defense_reinforcemen
 										if random.randint(0,2) == 0:
 											attacking_block.current_strength = 0
 
-									combat.attack_block(attacking_block, defense)
+									attack_block(attacking_block, defense)
 
 				
-									attacker_is_dead, defender_is_dead = combat.check_if_dead(attack, defense, attack_reinforcements, defense_reinforcements)
+									attacker_is_dead, defender_is_dead = check_if_dead(attack, defense, attack_reinforcements, defense_reinforcements)
 			
 									if defender_is_dead and combat_round != 0:
 
@@ -181,15 +312,8 @@ def pick_random_block(block_tuple, attack, defense, attack_reinforcements, defen
 
 	return block_to_be_attacked
 
-def print_situation(attack,defense):
-	print('attack:')
-	for block in attack:
-		print(block.current_strength, end = ' ')
-	print('\n')
-	print('defense:')
-	for block in defense:
-		print(block.current_strength, end = ' ')
-	print('\n')
+
+
 
 
 
