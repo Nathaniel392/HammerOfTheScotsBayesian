fa8d0557cf8b9bae3e7c7087773bf33973277ab3
ameliamylvaganam <41197477+ameliamylvaganam@users.noreply.github.com>
2018-08-02 15:13:52 -0400

    Update attacked_borders.py

--- attacked_borders.py
+++ attacked_borders.py
@@ -1,4 +1,8 @@
 def make_attacked_borders():
+	"""
+	makes attacked borders
+	set them all to False
+	"""
 	attacked_borders = list()
 	for i in range(23):
 		attacked_borders.append(list())
@@ -7,6 +11,9 @@ def make_attacked_borders():
 	return attacked_borders
 
 def reset_attacked_borders(attacked_borders):
+	"""
+	resets all attacked borders to False
+	"""
 	for i in range(23):
 		for j in range(23):
 			attacked_borders[i][j] = False
