900a0203e79b05ad6524e50710abfa28505755fb
David27737 <41197507+David27737@users.noreply.github.com>
2018-08-08 21:14:22 -0400

    Update board.py

--- board.py
+++ board.py
@@ -394,12 +394,7 @@ class Board(object):
 		#Final output
 
 
-		#debugging
-		for path in all_paths:
-			if type(path) != list:
-				print(all_paths)
-				print(path, type(path))
-				raise Exception('path not a list')
+		
 		return all_paths
 
 	def check_all_paths(self, num_moves, startID, block, path=[], stop=False, all_paths=[], truce=False, role = 'ENGLAND'):
