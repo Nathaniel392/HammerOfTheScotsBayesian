2d0ad199810b00a6c9c973f94c030a3d7c756f48
David27737 <41197507+David27737@users.noreply.github.com>
2018-08-16 14:09:19 -0400

    Add files via upload

--- update_roster.py
+++ update_roster.py
@@ -25,81 +25,84 @@ def update_roster(all_blocks_lst = None, current_board = None):
 		except AttributeError:
 			pass
 
+		try:	
+			if type(block) == blocks.Noble:
+
+				if block.has_cross and block.is_dead():
 			
-		if type(block) == blocks.Noble:
-
-			if block.has_cross and block.is_dead():
-		
-				
-				current_board.scot_roster.remove(block)
-
-				current_board.remove_from_region(block, find_location(current_board,block).regionID)
-				
-			else:
-
-
-				block.b2_to_b3(2)
-				if block.allegiance == 'SCOTLAND':
-					#if block.allegiace is SCOTLAND and can't find it then put it from the english one and into the scotland one for nobles
-					block_found_bool = False
-					for block2 in current_board.scot_roster:
-						if block is block2:
-							block_found_bool = True
-							break
-
-					if not block_found_bool:
-						for i, block2 in enumerate(current_board.eng_roster):
-							if block is block2:
-								current_board.scot_roster.append(current_board.eng_roster.pop(i))
-
-								break
-				elif block.allegiance == 'ENGLAND':
-					block_found_bool = False
-					for block2 in current_board.eng_roster:
-						if block is block2:
-							block_found_bool = True
-							break
-					if not block_found_bool:
-						for i, block2 in enumerate(current_board.scot_roster):
-							if block is block2:
-								current_board.eng_roster.append(current_board.scot_roster.pop(i))
-								break
-		elif block.is_dead() and not block.has_cross and (block in current_board.eng_roster or block in current_board.scot_roster):	
-			#print(block.name, ' is dead and we are looking for him')
-			if block.allegiance == 'SCOTLAND':
-				
-				if find_location(current_board, block):
-					current_board.scot_pool.append(current_board.remove_from_region(block, \
-						find_location(current_board, block).regionID))
-				elif block not in current_board.scot_pool:
-					current_board.scot_pool.append(block)
-				if block in current_board.scot_roster:
+					
 					current_board.scot_roster.remove(block)
 
-			elif block.allegiance == 'ENGLAND':
-				if find_location(current_board, block):
+					current_board.remove_from_region(block, find_location(current_board,block).regionID)
+					
+				else:
+
+
+					block.b2_to_b3(2)
+					if block.allegiance == 'SCOTLAND':
+						#if block.allegiace is SCOTLAND and can't find it then put it from the english one and into the scotland one for nobles
+						block_found_bool = False
+						for block2 in current_board.scot_roster:
+							if block is block2:
+								block_found_bool = True
+								break
+
+						if not block_found_bool:
+							for i, block2 in enumerate(current_board.eng_roster):
+								if block is block2:
+									current_board.scot_roster.append(current_board.eng_roster.pop(i))
+
+									break
+					elif block.allegiance == 'ENGLAND':
+						block_found_bool = False
+						for block2 in current_board.eng_roster:
+							if block is block2:
+								block_found_bool = True
+								break
+						if not block_found_bool:
+							for i, block2 in enumerate(current_board.scot_roster):
+								if block is block2:
+									current_board.eng_roster.append(current_board.scot_roster.pop(i))
+									break
+			elif block.is_dead() and not block.has_cross and (block in current_board.eng_roster or block in current_board.scot_roster):	
+				#print(block.name, ' is dead and we are looking for him')
+				if block.allegiance == 'SCOTLAND':
+					
+					if find_location(current_board, block):
+						current_board.scot_pool.append(current_board.remove_from_region(block, \
+							find_location(current_board, block).regionID))
+					elif block not in current_board.scot_pool:
+						current_board.scot_pool.append(block)
+					if block in current_board.scot_roster:
+						current_board.scot_roster.remove(block)
+
+				elif block.allegiance == 'ENGLAND':
+					if find_location(current_board, block):
+						current_board.eng_pool.append(current_board.remove_from_region(block, \
+							find_location(current_board, block).regionID))
+					elif block not in current_board.eng_pool:
+						current_board.eng_pool.append(block)
+					if block in current_board.eng_roster:
+						current_board.eng_roster.remove(block)
+
+			elif block.is_dead() and block.has_cross:
+
+				if block.type == 'EDWARD':
+					block.type = 'KING'
 					current_board.eng_pool.append(current_board.remove_from_region(block, \
 						find_location(current_board, block).regionID))
-				elif block not in current_board.eng_pool:
-					current_board.eng_pool.append(block)
-				if block in current_board.eng_roster:
 					current_board.eng_roster.remove(block)
 
-		elif block.is_dead() and block.has_cross:
-
-			if block.type == 'EDWARD':
-				block.type = 'KING'
-				current_board.eng_pool.append(current_board.remove_from_region(block, \
-					find_location(current_board, block).regionID))
-				current_board.eng_roster.remove(block)
-
-			else:
-				if find_location(current_board, block):
-					current_board.remove_from_region(block, find_location(current_board, block).regionID)
-				if block.allegiance == 'ENGLAND':
-					current_board.eng_roster.remove(block)
-				elif block.allegiance == 'SCOTLAND':
-					current_board.scot_roster.remove(block)
+				else:
+					if find_location(current_board, block):
+						current_board.remove_from_region(block, find_location(current_board, block).regionID)
+					if block.allegiance == 'ENGLAND':
+						current_board.eng_roster.remove(block)
+					elif block.allegiance == 'SCOTLAND':
+						current_board.scot_roster.remove(block)
+		except AttributeError:
+			#someone not found because they're dead or something or ghost idk 
+			continue
 
 
 	
